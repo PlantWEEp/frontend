83d4c5368bfa793ac85c0bab58faff5a753603a8
SonuRamesh <130997188+SonuRamesh@users.noreply.github.com>
2024-04-10 21:43:34 +0530

    zod validation added

--- src/component/auth/Login.jsx
+++ src/component/auth/Login.jsx
@@ -1,32 +1,76 @@
 import React from "react";
 import "./Login.css";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const adminLoginz = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(adminLoginz),
+  });
+
+  const submitData = (data) => {
+    console.log("IT WORKED", data);
+  };
+
   return (
-    <div class="container-fiuld blue-white">
-      <div class="row">
-        <div class="col-md-6 blue-bg"></div>
-        <div class="col-md-6 ">
-          <div class="d-flex justify-content-center align-items-center right-div">
-            <div class="center-box">
-              <h4 class="heading">Login to My Tution App</h4>
-              <h6 class="heading2">
+    <div className="container-fiuld blue-white">
+      <div className="row">
+        <div className="col-md-6 blue-bg"></div>
+        <div className="col-md-6 ">
+          <div className="d-flex justify-content-center align-items-center right-div">
+            <div className="center-box">
+              <h4 className="heading">Login to My Tution App</h4>
+              <h6 className="heading2">
                 Please enter your email and password to continue
               </h6>
-              <div class="input-group">
-                <label htmlFor="email">Email Address:</label>
-                <input type="email" id="email" placeholder="abc@gmail.com" />
-              </div>
-              <div class="input-group">
-                <div class="labeldiv"> 
-                  <label htmlFor="password">Password:</label>
-                  <span class="forgot-password">Forgot password?</span>
+              <form onSubmit={handleSubmit(submitData)}>
+                <div className="input-group">
+                  <label htmlFor="email">Email Address:</label>
+                  <input
+                    type="email"
+                    id="email"
+                    placeholder="abc@gmail.com"
+                    {...register("email")}
+                  />
+                  {errors.email && (
+                    <span className="error-message">
+                      {errors.email.message}
+                    </span>
+                  )}
                 </div>
-                <input type="password" id="password" placeholder="" />
-              </div>
-              <div class="input-group">
-                <button class="signin-button">Sign In</button>
-              </div>
+                <div className="input-group">
+                  <div className="labeldiv">
+                    <label htmlFor="password">Password:</label>
+                    <span className="forgot-password">Forgot password?</span>
+                  </div>
+                  <input
+                    type="password"
+                    id="password"
+                    placeholder=""
+                    {...register("password")}
+                  />
+                  {errors.password && (
+                    <span className="error-message">
+                      {errors.password.message}
+                    </span>
+                  )}
+                </div>
+                <div className="input-group">
+                  <button type="submit" className="signin-button">
+                    Sign In
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
