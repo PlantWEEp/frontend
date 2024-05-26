55b0f9cd018f51713fd68b7f2d8242a5eaf774e0
Vaishnav <vaishnav0931@gmail.com>
2024-05-26 17:22:19 +0530

    added api

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server : {
     proxy : {
-       '/api' : 'http://localhost:8000/'
+       '/api' : 'https://tuitionapp.onrender.com/'
     },
   },
   plugins: [react()],
